7a4c222b88b767b75663fadaae147fc3794c5b25
Ahmad Tufail <164312938+ahmadtufail1158@users.noreply.github.com>
2024-07-02 18:59:22 +0500

    done sho

---
+++ script.js
@@ -0,0 +1,57 @@
+// let str = " Hello World ";
+// str = str.trim();
+// console.log(str); Output: "Hello World"
+
+
+
+
+// let str = "Hello World";
+// let substr = str.substring(5, 10);
+// console.log(substr); // Output: "World"
+
+
+
+// let str = "Hello World";
+// let substr = str.substring(0, 5);
+// console.log(substr); // Output: "Hello"
+
+
+
+// let str = "hello-world";
+// let arr = str.split("-");
+// console.log(arr); // Output: ["hello", "world"]
+
+
+
+// let arr = ["hello", "world"];
+// let str = arr.join(" ");
+// console.log(str); // Output: "hello world"
+
+
+
+// let str = "Hello World";
+// let index = str.indexOf("o");
+// console.log(index); // Output: 4
+
+
+
+// let str = "Hello World World";
+// let index = str.lastIndexOf("World");
+// console.log(index); // Output: 12
+
+
+
+// let str = "World";
+// let startsWith = str.startsWith("Wor");
+// console.log(startsWith); // Output: true
+
+
+// let str = "Hello World";
+// let endsWith = str.endsWith("World");
+// console.log(endsWith); // Output: true
+
+
+
+// let str = "Hello";
+// let includes = str.includes("o");
+// console.log(includes); // Output: true
